90f08187c568f333b8500662256479ac1c269d83
Mualamx <Mualamx@163.com>
2025-10-27 11:43:11 +0800

    ✅ 关闭debug

--- core/database/userSoil.py
+++ core/database/userSoil.py
@@ -37,7 +37,7 @@ class CUserSoilDB(CSqlManager):
         Args:
             soilIndex (int): 地块索引 从1开始
         """
-        if g_bIsDebug:
+        if not g_bIsDebug:
             return
 
         soilInfo = await cls.getUserSoil(uid, soilIndex)
